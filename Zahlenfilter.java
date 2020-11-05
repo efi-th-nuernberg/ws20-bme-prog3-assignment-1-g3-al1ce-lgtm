917c81c9cb734cd4ab0989230485701620c3d158
al1ce-lgtm <73478909+al1ce-lgtm@users.noreply.github.com>
2020-11-05 17:38:19 +0100

    Zahlenfilter Aktuell

--- Zahlenfilter.java
+++ Zahlenfilter.java
@@ -1,5 +1,18 @@
 class Zahlenfilter {
   public static void main(String[] args) {
-    System.out.println("4");
+
+    for (int i = 0; i <= 200; i++){
+      if (i % 5 == 0){
+        System.out.println(i + " ist durch genau 5 teilbar!" );
+      }else{
+        if(i % 10 == 9){
+          System.out.println(i + " endet tatsächlich auf 9!");
+        }else{
+          if ((i + i - 1)%3 == 0) {
+            System.out.println(i + " und " + (i-1) + "zusammen addiert ergeben" + (i + i - 1) + " und " + (i + i - 1) + " ist durch 3 teilbar!");
+        }
+        }
+      }
+    }
   }
 }
